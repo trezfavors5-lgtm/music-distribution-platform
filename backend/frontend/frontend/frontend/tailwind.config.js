2c2691bca23272e519fb1a6cf185ac948b192210
trezfavors5-lgtm <trezfavors5@gmail.com>
2026-02-19 14:44:48 -0500

    Create tailwind.config.js

---
+++ backend/frontend/frontend/frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./app/**/*.{js,tsx}', './components/**/*.{js,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        dark: { 50: '#f9fafb', 100: '#f3f4f6', 200: '#e5e7eb', 800: '#1f2937', 900: '#111827', 950: '#030712' }
+      }
+    }
+  },
+  plugins: []
+}
